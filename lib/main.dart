818094e6c6fb570bf53d85f7efb836e76ce80897
Doug MacMillan <github@raette.com>
2024-09-30 17:03:04 -0700

    turn off debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'roadway'),
+      debugShowCheckedModeBanner: false, // Add this line
     );
   }
 }
